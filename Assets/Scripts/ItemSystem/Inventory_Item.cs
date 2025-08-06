d19a66eb0b30f94b20ad0e843be0f1b5953947f8
aubwils <128730648+aubwils@users.noreply.github.com>
2025-08-06 17:56:44 -0400

    WIP

--- Assets/Scripts/ItemSystem/Inventory_Item.cs
+++ Assets/Scripts/ItemSystem/Inventory_Item.cs
@@ -9,10 +9,41 @@ public class Inventory_Item
     public ItemDataSO itemData;
     public int stackSize = 1; // default stack size when creating it is one by default
 
+    public ItemModifier[] modifiers { get; private set; }
+
     //need constructor so can create item and add to inventory
     public Inventory_Item(ItemDataSO itemData)
     {
         this.itemData = itemData;
+
+        modifiers = EquipmentData()?.modifiers;
+    }
+
+    public void AddModifiers(Entity_Stats playerstats)
+    {
+        foreach (var mod in modifiers)
+        {
+            Stat statToModify = playerstats.GetStatByType(mod.statType);
+            statToModify.AddModifier(mod.value, itemData.itemName); // will need to change this cant just use item name because dont want to be bale to use same item with the same name. for now ok
+        }
+    }
+
+    public void RemoveModifiers(Entity_Stats playerstats)
+    {
+        foreach (var mod in modifiers)
+        {
+            Stat statToModify = playerstats.GetStatByType(mod.statType);
+            statToModify.RemoveModifier(itemData.itemName); 
+
+        }
+    }
+
+    private EquipmentDataSO EquipmentData()
+    {
+        if (itemData is EquipmentDataSO equipment)
+            return equipment;
+
+        return null;
     }
 
     public bool CanAddStack() => stackSize < itemData.maxStackSize;
